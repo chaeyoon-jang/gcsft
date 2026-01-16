914e71ec3fe605a141c118257cca8131bd4de14b
chaeyoon-jang <wkdcodbs9911@gmail.com>
2026-01-16 20:15:05 +0900

    <add> some details

---
+++ scripts/llama/train/single_gsm.sh
@@ -0,0 +1,11 @@
+python -m experiments.train.train_csft \
+  --model_name meta-llama/Llama-3.2-3B-Instruct \
+  --train_type single_gsm \
+  --batch_size 2 \
+  --gradient_accumulation_steps 32 \
+  --learning_rate 1e-04 \
+  --use_lora \
+  --confidence_key conf_label_single \
+  --confidence_input_key conf_input_single \
+  --kl_decay 0.0 \
+  --use_wandb 
